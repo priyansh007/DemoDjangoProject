f86b3adb96ed207c43441f6ed06720899fd99cff
Priyansh Zalavadiya <priyanshzalavadiya007@gmail.com>
2023-08-09 00:29:57 -0700

    message sent

--- demoProject/firstApp/models.py
+++ demoProject/firstApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Text(models.Model):
-	text = models.CharField(max_length = 500, blank = False)
+	text = models.CharField(max_length = 500, null=True)
 	time_sent = models.DateTimeField(auto_now_add = True) 
